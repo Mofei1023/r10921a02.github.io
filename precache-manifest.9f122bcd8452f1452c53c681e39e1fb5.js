6d0b2e5fe5cac39dafb7d10e4f1ed322090e408c
Mofei1023 <71375565+Mofei1023@users.noreply.github.com>
2023-04-10 01:28:45 +0800

    Updates

---
+++ precache-manifest.9f122bcd8452f1452c53c681e39e1fb5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe63d61eff94404676a99ce4801893d",
+    "url": "/r10921a02/index.html"
+  },
+  {
+    "revision": "e53ce6f05dde455c90be",
+    "url": "/r10921a02/static/css/main.fb6fec5e.chunk.css"
+  },
+  {
+    "revision": "7a737f96ba7be86bbd89",
+    "url": "/r10921a02/static/js/2.32dceade.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/r10921a02/static/js/2.32dceade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e53ce6f05dde455c90be",
+    "url": "/r10921a02/static/js/main.ccb83996.chunk.js"
+  },
+  {
+    "revision": "633f6a511d514a4559ad",
+    "url": "/r10921a02/static/js/runtime-main.051689d5.js"
+  },
+  {
+    "revision": "b6d609dc432d2fc28e42e1dd906f1acd",
+    "url": "/r10921a02/static/media/ethan.b6d609dc.jpg"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/r10921a02/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/r10921a02/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/r10921a02/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/r10921a02/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/r10921a02/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/r10921a02/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "27e2aa9f10973de649ddef5c90196b5e",
+    "url": "/r10921a02/static/media/profile01.27e2aa9f.jpg"
+  },
+  {
+    "revision": "ba857ba2fa123e812b19aa152f1d1802",
+    "url": "/r10921a02/static/media/profile02.ba857ba2.jpg"
+  },
+  {
+    "revision": "bbb9af5c15fdddbd95b53597bf69fe3d",
+    "url": "/r10921a02/static/media/profile03.bbb9af5c.jpg"
+  },
+  {
+    "revision": "4209d83b19b05d70219c0d93fb8fc2f6",
+    "url": "/r10921a02/static/media/profile04.4209d83b.jpg"
+  }
+]);
